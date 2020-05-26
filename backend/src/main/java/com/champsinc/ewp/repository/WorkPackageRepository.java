e0ce753f9b61a26215a30d0f0adc8e1283cd7f8b
Dhiren Chandnani <dhirensc65@gmail.com>
2020-05-26 13:16:52 -0400

    Revert "Initial rest api setup"
    This reverts commit 02ba94a753a74d3f9fc0851d9e4dfaff6383f40b.

--- backend/src/main/java/com/champsinc/ewp/repository/WorkPackageRepository.java
+++
@@ -1,8 +0,0 @@
-package com.champsinc.ewp.repository;
-
-import com.champsinc.ewp.model.WorkPackage;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface WorkPackageRepository extends MongoRepository<WorkPackage, String> {
-    WorkPackage findByewpId(int ewpId);
-}
