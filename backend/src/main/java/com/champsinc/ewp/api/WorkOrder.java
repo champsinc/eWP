02ba94a753a74d3f9fc0851d9e4dfaff6383f40b
dhiren <dhirensc65@gmail.com>
2020-05-26 11:00:01 -0400

    Initial rest api setup

--- backend/src/main/java/com/champsinc/ewp/api/WorkOrder.java
+++ backend/src/main/java/com/champsinc/ewp/api/WorkOrder.java
@@ -1,11 +1,29 @@
 package com.champsinc.ewp.api;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.champsinc.ewp.model.WorkPackage;
+import com.champsinc.ewp.repository.WorkPackageRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/api")
 public class WorkOrder {
+    @Autowired
+    private WorkPackageRepository repository;
+
     @GetMapping("/wo/details")
     public String greeting() {
         return "Work Order Details Here";
+    }
+
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public List<WorkPackage> getAllWorkPackages() {
+        return repository.findAll();
+    }
+
+    @RequestMapping(value = "/{ewpId}", method = RequestMethod.GET)
+    public WorkPackage getPetByEwpId(@PathVariable("ewpId") int ewpId) {
+        return repository.findByewpId(ewpId);
     }
 }
