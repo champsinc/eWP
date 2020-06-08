9c73edfe98cd7bf6038e83fa989c91947c1678e4
Raghul Krishnan <raghul.sathya@gmail.com>
2020-06-08 13:30:52 -0400

    Subsection mapper initial cut

---
+++ frontend/components/SubSectionMaper.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { View } from "react-native";
+
+export class SubSectionMapper extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <View>
+        <Text></Text>
+      </View>
+    );
+  }
+}
