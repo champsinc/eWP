ea880b9a4b8065e0b1d7be5986fa587995c770a7
Raghul Krishnan <raghul.sathya@gmail.com>
2020-06-08 13:01:55 -0400

    cleanup unused dependencies and remove filepond imports

--- frontend/components/FilePondModal.js
+++ frontend/components/FilePondModal.js
@@ -1,19 +1,6 @@
 import React from "react";
-import { Portal, Dialog, Paragraph } from "react-native-paper";
-import { FilePond, registerPlugin } from "react-filepond";
-import "filepond/dist/filepond.min.css";
-import FilePondPluginImageExifOrientation from "filepond-plugin-image-exif-orientation";
-import FilePondPluginImagePreview from "filepond-plugin-image-preview";
-import "filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css";
-import FilePondPluginFileValidateType from "filepond-plugin-file-validate-type";
-import FilePondPluginFileValidateSize from "filepond-plugin-file-validate-size";
+import { Portal, Dialog } from "react-native-paper";
 
-registerPlugin(
-  FilePondPluginImageExifOrientation,
-  FilePondPluginImagePreview,
-  FilePondPluginFileValidateType,
-  FilePondPluginFileValidateSize
-);
 export default class FilePondModal extends React.Component {
   constructor(props) {
     super(props);
@@ -31,23 +18,7 @@ export default class FilePondModal extends React.Component {
           onDismiss={this.props.onModalClose}
         >
           <Dialog.Title>Attach Files</Dialog.Title>
-          <Dialog.Content>
-            <FilePond
-              ref={(ref) => (this.pond = ref)}
-              acceptedFileTypes={["image/png", "image/jpg", "image/jpeg"]}
-              labelIdle={"Upload files.."}
-              allowMultiple={false}
-              //   oninit={() => this.handleInit()}
-              //   maxFileSize={"10MB"}
-              //   allowFileSizeValidation={true}
-              server={{
-                process: {
-                  url: "UPLOAD_MEDIA",
-                  onload: (response) => this.loaded(response),
-                },
-              }}
-            />
-          </Dialog.Content>
+          <Dialog.Content></Dialog.Content>
         </Dialog>
       </Portal>
     );
