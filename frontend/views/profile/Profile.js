82c248fffef81040702aa85a2e8003da27a6017c
Raghul Krishnan <raghul.sathya@gmail.com>
2020-09-09 11:34:21 -0400

    profile screen updates

--- frontend/views/profile/Profile.js
+++ frontend/views/profile/Profile.js
@@ -1,7 +1,7 @@
 // author: nirbhay pherwani | pherwani37@gmail.com  | https://nirbhay.me
 import React from "react";
 import { View, StyleSheet, Platform, Dimensions, Text } from "react-native";
-import { Button, Avatar, Card } from "react-native-paper";
+import { Button, Avatar, Card, TextInput } from "react-native-paper";
 import AppBar from "./../../components/AppBar";
 import { AuthContext } from "../../App";
 
@@ -16,6 +16,9 @@ export default class Profile extends React.Component {
     super(props);
     this.state = {
       logout: <View />,
+      name: this.props.user.name,
+      email: this.props.user.email,
+      saveButtonDisabled: true,
     };
   }
 
@@ -36,6 +39,38 @@ export default class Profile extends React.Component {
     );
   };
 
+  onChangeTextName = (name) => {
+    this.setState({
+      name,
+      saveButtonDisabled:
+        this.props.user.name == name &&
+        this.props.user.email == this.state.email,
+    });
+  };
+
+  onChangeTextEmail = (email) => {
+    this.setState({
+      email,
+      saveButtonDisabled:
+        this.props.user.email == email &&
+        this.props.user.name == this.state.name,
+    });
+  };
+
+  onTextInputFocus = () => {
+    // Code to eliminate the outline for a textinput
+    this.name.setNativeProps({
+      style: {
+        outline: "none",
+      },
+    });
+    this.email.setNativeProps({
+      style: {
+        outline: "none",
+      },
+    });
+  };
+
   render() {
     return (
       <View style={{ flex: 1 }}>
@@ -48,15 +83,49 @@ export default class Profile extends React.Component {
               label={this.extractInitialsFromName(this.props.user.name)}
               size={50}
             />
-            <Text style={styles.text}>{this.props.user.name}</Text>
-            <Button
-              uppercase={false}
-              style={styles.logoutButton}
-              onPress={this.logout}
-              labelStyle={styles.logoutButtonText}
-            >
-              Logout
-            </Button>
+            <View style={styles.textButtonWrapper}>
+              <Text style={styles.labelStyle}>Username:</Text>
+              <TextInput
+                ref={(ref) => (this.name = ref)}
+                style={styles.textInput}
+                onFocus={this.onTextInputFocus}
+                value={this.state.name}
+                placeholder={"Username"}
+                mode={"outlined"}
+                onChangeText={this.onChangeTextName}
+              />
+            </View>
+            <View style={styles.textButtonWrapper}>
+              <Text style={styles.labelStyle}>Email:</Text>
+              <TextInput
+                ref={(ref) => (this.email = ref)}
+                style={styles.textInput}
+                onFocus={this.onTextInputFocus}
+                placeholder={"Email"}
+                mode={"outlined"}
+                value={this.state.email}
+                onChangeText={this.onChangeTextEmail}
+              ></TextInput>
+            </View>
+            <View style={[styles.textButtonWrapper, { width: "75%" }]}>
+              <Button
+                uppercase={false}
+                style={styles.saveButton}
+                onPress={() => {}}
+                labelStyle={styles.saveButtonText}
+                disabled={this.state.saveButtonDisabled}
+              >
+                Save
+              </Button>
+              <Button
+                uppercase={false}
+                style={styles.logoutButton}
+                onPress={this.logout}
+                labelStyle={styles.logoutButtonText}
+              >
+                Logout
+              </Button>
+            </View>
           </Card>
         </View>
       </View>
@@ -74,25 +143,42 @@ const styles = StyleSheet.create({
   },
   card: {
     paddingVertical: 20,
-    width: "25%",
+    width: "35%",
     minWidth: 220,
     alignSelf: "center",
   },
   avatar: {
     alignSelf: "center",
   },
-  text: {
+  textInput: {
     alignSelf: "center",
     marginTop: 10,
     fontSize: 16,
     fontWeight: "bold",
   },
+  labelStyle: {
+    alignSelf: "center",
+    fontSize: 16,
+  },
   logoutButton: {
-    flexDirection: "row",
+    alignSelf: "center",
+    marginTop: 10,
+  },
+  saveButton: {
     alignSelf: "center",
     marginTop: 20,
   },
+  saveButtonText: {
+    fontSize: 16,
+  },
   logoutButtonText: {
     fontSize: 16,
+  },
+  textButtonWrapper: {
+    flexDirection: "row",
+    alignSelf: "center",
+    width: "70%",
+    justifyContent: "space-between",
+    padding: 10,
   },
 });
